6b87206b57af76ec84d302a8d811b09d4a805e4a
YeSeul-0w0 <choi333ys@gmail.com>
2024-10-06 21:32:29 +0900

    feat: temp structure of sidebar

--- src/component/layout/Sidebar.tsx
+++ src/component/layout/Sidebar.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Flex,
+	Text,
+	VStack
+} from "@chakra-ui/react";
 
 const Sidebar: React.FC = () => {
 	return (
 		<Box width="100%" height="100vh" p={4}>
-			<Text>알고 풀자</Text>
+			<Flex height="100vh">
+				<Box width="200px" boxShadow="lg" p={4}>
+					<VStack spacing={4} align="stretch">
+						<Box fontSize="2xl" fontWeight="bold" mb={4}>
+							타이틀
+						</Box>
+						<Button variant="ghost">A</Button>
+						<Button variant="ghost">B</Button>
+						<Button variant="ghost">C</Button>
+						<Button variant="ghost">D</Button>
+						<Button
+							variant="solid"
+							colorScheme="red"
+							mt="auto"
+						>
+							로그아웃
+						</Button>
+					</VStack>
+				</Box>
+			</Flex>
 		</Box>
 	);
 };
