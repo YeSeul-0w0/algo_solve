a80ecb5e9f33b5cc6ab8542fa6540069863cd8e2
YeSeul-0w0 <choi333ys@gmail.com>
2024-10-06 22:33:43 +0900

    feat: apply to extend theme in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Router from "./component/Router";
+import ThemeColor from "./config/ThemeColor";
 
 function App() {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={ThemeColor}>
 			<Router />
 		</ChakraProvider>
 	);
