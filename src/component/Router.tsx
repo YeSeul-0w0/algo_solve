9478a495fd46052ef15d927c731ecc3fbfeeb5f4
YeSeul-0w0 <choi333ys@gmail.com>
2024-10-08 23:24:22 +0900

    feat: create temporay loading page

--- src/component/Router.tsx
+++ src/component/Router.tsx
@@ -1,16 +1,14 @@
-import {
-	BrowserRouter,
-	Routes,
-	Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Main from "./Main";
+import Loading from "./Loading";
 
 const Router: React.FC = () => {
 	return (
 		<BrowserRouter basename="algo-solve">
 			<Routes>
-				<Route path="/" element={<Main />} />
+				<Route path="/" element={<Loading />} />
+				<Route path="/main" element={<Main />} />
 			</Routes>
 		</BrowserRouter>
 	);
