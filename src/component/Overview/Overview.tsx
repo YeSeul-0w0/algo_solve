a4b7c3385f3f83884fb2f0c551dfc7062ae9ec93
YeSeul-0w0 <choi333ys@gmail.com>
2024-10-13 14:57:57 +0900

    feat: basic structure of overview left side

--- src/component/Overview/Overview.tsx
+++ src/component/Overview/Overview.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Title from "../Title/Title";
+import LeftOverview from "./LeftOverview";
 
 const Overview: React.FC = () => {
 	return (
-		<Grid templateColumns="repeat(2, 1fr)" templateRows="1fr 10fr">
+		<Grid
+			templateColumns="repeat(2, 1fr)"
+			templateRows="1fr 10fr"
+			height="100%"
+			width="100%"
+		>
 			<GridItem colSpan={2} marginTop={9} marginLeft={3} marginBottom={3}>
 				<Title description="스터디 정보를 확인하세요." />
 			</GridItem>
-			<GridItem>Left</GridItem>
+			<GridItem>
+				<LeftOverview />
+			</GridItem>
 			<GridItem>Right</GridItem>
 		</Grid>
 	);
