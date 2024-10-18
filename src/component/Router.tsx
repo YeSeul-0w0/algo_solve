38e72f3b257e79a0f8c69a092af24fcb646c95b5
YeSeul-0w0 <choi333ys@gmail.com>
2024-10-18 18:35:20 +0900

    fixed: resolve conflict

--- src/component/Router.tsx
+++ src/component/Router.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Main from "./Main";
-import Loading from "./Loading";
+import Account from "./layout/Account";
+import Login from "./account/Login";
 
 const Router: React.FC = () => {
 	return (
 		<BrowserRouter basename="algo_solve">
 			<Routes>
-				<Route path="/" element={<Loading />} />
-				<Route path="/main" element={<Main />}></Route>
+				<Route path="/" element={<Account />}>
+					<Route index element={<Login />} />
+				</Route>
+				<Route path="/main" element={<Main />} />
 			</Routes>
 		</BrowserRouter>
 	);
