ecc6d21c7caf1424508f19ccd8897778dc4d3a15
YeSeul-0w0 <choi333ys@gmail.com>
2024-10-18 19:09:15 +0900

    feat: create context

--- src/component/Router.tsx
+++ src/component/Router.tsx
@@ -3,17 +3,20 @@ import React from "react";
 import Main from "./Main";
 import Account from "./layout/Account";
 import Login from "./account/Login";
+import { UserProvider } from "./context/UserContext";
 
 const Router: React.FC = () => {
 	return (
-		<BrowserRouter basename="algo_solve">
-			<Routes>
-				<Route path="/" element={<Account />}>
-					<Route index element={<Login />} />
-				</Route>
-				<Route path="/main" element={<Main />} />
-			</Routes>
-		</BrowserRouter>
+		<UserProvider>
+			<BrowserRouter basename="algo_solve">
+				<Routes>
+					<Route path="/" element={<Account />}>
+						<Route index element={<Login />} />
+					</Route>
+					<Route path="/main" element={<Main />} />
+				</Routes>
+			</BrowserRouter>
+		</UserProvider>
 	);
 };
 
