54e6bd9175e2ef8c354873d8752b178233918691
YeSeul-0w0 <choi333ys@gmail.com>
2024-12-01 14:17:30 +0900

    update: handling overflow

--- src/component/overview/Overview.tsx
+++ src/component/overview/Overview.tsx
@@ -10,6 +10,7 @@ const Overview: React.FC = () => {
 			templateColumns="repeat(2, 1fr)"
 			templateRows="1fr 10fr"
 			height="100vh"
+			overflow="scroll"
 		>
 			<GridItem colSpan={2} marginTop={9} marginLeft={3} marginBottom={3}>
 				<Title description="스터디 정보를 확인하세요."></Title>
