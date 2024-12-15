5a495d61f5fb04c81cba360bc308a0485c0f60b5
YeSeul-0w0 <choi333ys@gmail.com>
2024-12-15 16:57:05 +0900

    update: deactivate token part

--- src/api/DefaultAPI.ts
+++ src/api/DefaultAPI.ts
@@ -1,19 +1,20 @@
 import axios from "axios";
 
 const DefaultAPI = axios.create({
-	baseURL: "https://temp.com", // 서버의 기본 URL
+	baseURL: "http://localhost:8080", // 서버의 기본 URL
 	timeout: 5000,
 	headers: {
 		"Content-Type": "application/json"
 	}
 });
 
-DefaultAPI.interceptors.request.use(config => {
-	const token = localStorage.getItem("token");
-	if (token) {
-		config.headers.Authorization = `Bearer ${token}`;
-	}
-	return config;
-});
+// Token 안넣음
+// DefaultAPI.interceptors.request.use(config => {
+// 	const token = localStorage.getItem("token");
+// 	if (token) {
+// 		config.headers.Authorization = `Bearer ${token}`;
+// 	}
+// 	return config;
+// });
 
 export default DefaultAPI;
