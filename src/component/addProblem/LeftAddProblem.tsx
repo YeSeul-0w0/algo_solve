691b584ae1445f9bca28d2d720d15501a5f804f0
YeSeul-0w0 <choi333ys@gmail.com>
2025-03-08 17:39:33 +0900

    update: add problem id & update level logic

--- src/component/addProblem/LeftAddProblem.tsx
+++ src/component/addProblem/LeftAddProblem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
 	Box,
 	Grid,
@@ -10,67 +10,71 @@ import {
 	Text,
 	Select,
 	FormLabel,
-	Button,
-	Flex
+	Button
 } from "@chakra-ui/react";
 import CalculatingDate from "../../utils/CalculatingDate";
-import levelInfo from "../../data/staic_data.json";
-
-interface LevelType {
-	level: {
-		backjoon: string[];
-		programmers: string[];
-	};
-}
+import platformInfo from "../../data/staic_data.json";
 
 interface Problem {
 	platform: string;
-	name: string;
-	link: string;
-	level: string;
+	problemId: string;
+	problemName: string;
+	problemLink: string;
+	mainLevel: string;
+	subLevel?: string | null;
 }
+
+const textStyle = () => ({
+	fontWeight: "semibold",
+	fontSize: "md",
+	alignContent: "center",
+	marginBottom: 3
+});
+
+const btnStyle = () => ({
+	variant: "solid",
+	bg: "deepBeige",
+	_hover: { bg: "beige" }
+});
 
 const LeftAddProblem: React.FC = () => {
 	const { current } = CalculatingDate();
 
-	const textStyle = () => ({
-		fontWeight: "semibold",
-		fontSize: "md",
-		alignContent: "center",
-		marginBottom: 3
-	});
-
-	const btnStyle = () => ({
-		variant: "solid",
-		bg: "deepBeige",
-		_hover: { bg: "beige" }
-	});
-
 	const [platform, setPlatform] =
-		React.useState<keyof LevelType["level"]>("backjoon");
-	const [name, setName] = React.useState<string>("");
-	const [link, setLink] = React.useState<string>("");
-	const [level, setLevel] = React.useState<string>("");
+		React.useState<keyof (typeof platformInfo)["platformInfo"]>("boj");
+	const [problemId, setProblemId] = React.useState<string>("");
+	const [problemName, setProblemName] = React.useState<string>("");
+	const [problemLink, setProblemLink] = React.useState<string>("");
+	const [mainLevel, setMainLevel] = React.useState<string>("");
+	const [subLevel, setSubLevel] = React.useState<string | null>(null);
 
-	const [options, setOptions] = React.useState<string[]>([]);
-	useEffect(() => {
-		setOptions(levelInfo.level[platform]);
-	}, [platform]);
+	const mainLevelOptions = platformInfo.platformInfo[platform].mainLevel;
+	const subLevelOptions =
+		platform === "boj" ? platformInfo.platformInfo.boj.subLevel : [];
 
 	const [problems, setProblems] = React.useState<Problem[]>([]);
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
 
-		const newProblem: Problem = { platform, name, link, level };
+		const newProblem: Problem = {
+			platform,
+			problemName: problemName,
+			problemId: problemId,
+			problemLink: problemLink,
+			mainLevel: mainLevel,
+			subLevel: subLevel
+		};
+
 		setProblems([...problems, newProblem]);
-
-		setPlatform("backjoon"); // 기본 플랫폼으로 설정
-		setName(""); // 문제명 초기화
-		setLink(""); // 링크 초기화
-		setLevel(""); // 레벨 초기화
+		console.log(problems);
+		setPlatform("boj");
+		setProblemName("");
+		setProblemId("");
+		setProblemLink("");
+		setMainLevel("");
+		setSubLevel(null);
 	};
-
 	return (
 		<Box m={5}>
 			<Box>
@@ -79,48 +83,59 @@ const LeftAddProblem: React.FC = () => {
 				</Text>
 				<form onSubmit={handleSubmit}>
 					<Grid
-						templateColumns="repeat(4, 1fr)"
-						templateRows="repeat(7, 1fr)"
+						templateColumns="repeat(3, 1fr)"
+						templateRows="repeat(10, 1fr)"
 						p={5}
 						gap={3}
 					>
-						<GridItem colSpan={2} rowSpan={2}>
+						<GridItem colSpan={3} rowSpan={2}>
 							<FormLabel {...textStyle()}>플랫폼</FormLabel>
 							<RadioGroup
-								defaultValue="backjoon"
+								defaultValue="boj"
 								value={platform}
 								onChange={value =>
-									setPlatform(value as keyof LevelType["level"])
+									setPlatform(
+										value as keyof (typeof platformInfo)["platformInfo"]
+									)
 								}
 							>
 								<HStack justifyContent="space-around">
-									<Radio
-										value="backjoon"
-										_checked={{
-											bg: "deepBeige",
-											borderColor: "beige"
-										}}
-									>
-										백준
-									</Radio>
-									<Radio
-										value="programmers"
-										_checked={{
-											bg: "navy",
-											borderColor: "gray"
-										}}
-									>
-										프로그래머스
-									</Radio>
+									{Object.entries(platformInfo.platformInfo).map(
+										([key, value]) => (
+											<Radio
+												key={key}
+												value={key}
+												_checked={{
+													bg: value.backgroundColor,
+													borderColor: value.borderColor
+												}}
+											>
+												{key === "boj"
+													? "백준"
+													: key === "programmers"
+														? "프로그래머스"
+														: "릿코드"}
+											</Radio>
+										)
+									)}
 								</HStack>
 							</RadioGroup>
 						</GridItem>
-						<GridItem colSpan={2} rowSpan={2} gap={3}>
+						<GridItem colSpan={3} rowSpan={2} gap={3}>
 							<FormLabel {...textStyle()}>문제명</FormLabel>
 							<Input
 								type="text"
-								value={name}
-								onChange={e => setName(e.target.value)}
+								value={problemName}
+								onChange={e => setProblemName(e.target.value)}
+							/>
+						</GridItem>
+
+						<GridItem colSpan={3} rowSpan={2} gap={3}>
+							<FormLabel {...textStyle()}>문제번호</FormLabel>
+							<Input
+								type="text"
+								value={problemId}
+								onChange={e => setProblemId(e.target.value)}
 							/>
 						</GridItem>
 
@@ -128,25 +143,59 @@ const LeftAddProblem: React.FC = () => {
 							<Text {...textStyle()}>링크</Text>
 							<Input
 								type="text"
-								value={link}
-								onChange={e => setLink(e.target.value)}
+								value={problemLink}
+								onChange={e => setProblemLink(e.target.value)}
 							></Input>
 						</GridItem>
 
-						<GridItem colSpan={4} rowSpan={2}>
-							<Text {...textStyle()}>레벨</Text>
-							<Select
-								placeholder="레벨을 선택하세요."
-								value={level || ""}
-								onChange={e => setLevel(e.target.value)}
-							>
-								{options.map((option, index) => (
-									<option key={index} value={option}>
-										{option}
-									</option>
-								))}
-							</Select>
-						</GridItem>
+						{platform === "boj" ? (
+							<>
+								<GridItem colSpan={2} rowSpan={2}>
+									<Text {...textStyle()}>난이도 선택</Text>
+									<Select
+										placeholder="난이도 선택"
+										value={mainLevel}
+										onChange={e => setMainLevel(e.target.value)}
+									>
+										{mainLevelOptions.map((option, index) => (
+											<option key={index} value={option}>
+												{option}
+											</option>
+										))}
+									</Select>
+								</GridItem>
+
+								<GridItem colSpan={1} rowSpan={2}>
+									<Text {...textStyle()}>숫자 선택</Text>
+									<Select
+										placeholder="숫자 선택"
+										value={subLevel || ""}
+										onChange={e => setSubLevel(e.target.value)}
+									>
+										{subLevelOptions.map((option, index) => (
+											<option key={index} value={option}>
+												{option}
+											</option>
+										))}
+									</Select>
+								</GridItem>
+							</>
+						) : (
+							<GridItem colSpan={3} rowSpan={2}>
+								<Text {...textStyle()}>레벨</Text>
+								<Select
+									placeholder="레벨 선택"
+									value={mainLevel}
+									onChange={e => setMainLevel(e.target.value)}
+								>
+									{mainLevelOptions.map((option, index) => (
+										<option key={index} value={option}>
+											{option}
+										</option>
+									))}
+								</Select>
+							</GridItem>
+						)}
 						<GridItem colSpan={4} display="flex" justifyContent="flex-end">
 							<Button {...btnStyle()} type="submit">
 								{" "}
@@ -156,12 +205,12 @@ const LeftAddProblem: React.FC = () => {
 					</Grid>
 				</form>
 			</Box>
-			<Box mt={5} border="1px" borderColor="beige" borderRadius={15} p={6}>
-				<Flex gap={9}>
-					<Input type="text" placeholder="문제 풀이 여부를 검색하세요" />
-					<Button {...btnStyle()}> 검색 </Button>
-				</Flex>
-			</Box>
+			{/*<Box mt={5} border="1px" borderColor="beige" borderRadius={15} p={6}>*/}
+			{/*	<Flex gap={9}>*/}
+			{/*		<Input type="text" placeholder="문제 풀이 여부를 검색하세요" />*/}
+			{/*		<Button {...btnStyle()}> 검색 </Button>*/}
+			{/*	</Flex>*/}
+			{/*</Box>*/}
 		</Box>
 	);
 };
