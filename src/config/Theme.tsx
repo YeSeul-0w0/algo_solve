c78794d9e749657063e480701fa035eb743e59a4
YeSeul-0w0 <choi333ys@gmail.com>
2025-03-08 17:23:58 +0900

    chore: add color

--- src/config/Theme.tsx
+++ src/config/Theme.tsx
@@ -9,7 +9,9 @@ const Theme = extendTheme({
 		deepBlue: "#0F2C96",
 		black: "#000000",
 		lightGray: "#9A9A9A",
-		brown: "#AD8B73"
+		brown: "#AD8B73",
+		yellow: "#F7C566",
+		orange: "#DC6B19"
 	},
 	fonts: {
 		body: `'Outfit', sans-serif` // 본문에 사용할 폰트
